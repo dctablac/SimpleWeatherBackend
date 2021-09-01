5f02b0a74605e2ffd1fae7dbd449a63437087212
Dan Christopher Tablac <dctablac@gmail.com>
2021-08-31 21:29:47 -0700

    Endpoint for retrieving weather details

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/weather")
+def get_weather():
+    return 'GOT YOUR WEATHER'
+
 @app.route("/")
 def hello_world():
     return "HELLO WORLD"
