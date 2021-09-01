1eb1600e2d138b7ddfdc52e9fd95064debd76150
Dan Christopher Tablac <dctablac@gmail.com>
2021-08-31 21:56:12 -0700

    Access to open weather api from this endpoint. env variable use added.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from dotenv import dotenv_values
+import requests
+
+config = dotenv_values('.env')
 
 app = Flask(__name__)
 
 @app.route("/weather")
 def get_weather():
-    return 'GOT YOUR WEATHER'
+    api_url = 'http://api.openweathermap.org/data/2.5/forecast?q='
+    city = 'Irvine'
+    api_string = api_url + city + '&appid=' + config["API_KEY"]
+    return requests.get(api_string).json()
 
 @app.route("/")
 def hello_world():
