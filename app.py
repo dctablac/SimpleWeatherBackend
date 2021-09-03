e15179bdbe2c9dfe3e680d692a0a2062e1c0f4c1
Dan Christopher Tablac <dctablac@gmail.com>
2021-09-02 21:47:42 -0700

    Altered cross origin decorator

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from weather import weather_details
 app = Flask(__name__)
 
 @app.route('/weather/<city>', methods=['GET'])
-@cross_origin(origins=['http://localhost:8080', 'https://dctablac-simple-weather.herokuapp.com/'])
+@cross_origin(origins=['http://localhost:8080', 'https://dctablac-simple-weather.herokuapp.com'])
 def get_weather(city):
     return make_response(weather_details(city))
 
